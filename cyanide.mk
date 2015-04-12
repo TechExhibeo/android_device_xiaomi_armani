7f56d413624665605174fb87f7a4668ee912f351
Deepak Sharma <TechExhibeo@gmail.com>
2015-04-12 10:39:05 +0530

    cyanide-ify

--- cyanide.mk
+++ cyanide.mk
@@ -1,9 +1,9 @@
 # Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cyanide/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/armani/full_armani.mk)
 
-PRODUCT_NAME := cm_armani
+PRODUCT_NAME := cyanide_armani
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
