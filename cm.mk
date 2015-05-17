e1d0dab00dcfe12cb2fb3c5667638708629aca62
Deepak Sharma <techexhibeo@gmail.com>
2015-05-17 02:31:45 +0200

    merge

--- cm.mk
+++
@@ -1,9 +0,0 @@
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/xiaomi/armani/full_armani.mk)
-
-PRODUCT_NAME := cm_armani
-
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
